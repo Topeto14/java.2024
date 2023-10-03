465c09033cfafd84b5144dcdbbe0856afe7916c9
Topeto14 <146084604+Topeto14@users.noreply.github.com>
2023-10-03 20:52:19 +0200

    Add files via upload

---
+++ ParImpar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    import java.util.Scanner;
+        //1. Declaración de variables
+        int num;
+
+        //2. Leer de teclado esa variable
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        
+        //3. Comprobar si es par o impar
+        if (num % 2 == 0) {
+            System.out.println(" Es par ");
+        } else {
+            System.out.println(" Es impar ");
+        }
+
+        if (num % 10 == 0) {
+            System.out.println(" Es multiplo de 10");
+        } else {
+            System.out.println(" No es multiplo de 10");
+        }
+
+        
+
+    }
+}
