e9d8f63dc653cfddabeeed1d2118acf28a1a4b21
Topeto14 <146084604+Topeto14@users.noreply.github.com>
2023-10-18 18:38:22 +0200

    Add files via upload

---
+++ ejercicioEntregar10.java
@@ -0,0 +1,29 @@
+public class ejercicioEntregar10 {
+
+    public static void main(String[] args) {
+        
+        //10. Programa (utilizando bucles) que muestre por pantalla la siguiente figura. Debe funcionar para n =
+        //número de líneas, que se pida por teclado.
+        //*
+        //**
+        //***
+        //****
+
+
+        int num =4 ;
+
+        for (int i=1; i<=num; i++ ){
+            for(int j=num; j>=i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+    
+}
