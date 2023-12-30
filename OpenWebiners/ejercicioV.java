7d66efe4fccf738d7624f8bec6dbdde0fe5129e0
Topeto14 <levishurtadorobert@gmail.com>
2023-12-30 15:14:22 +0100

    fdghgfdh

---
+++ OpenWebiners/ejercicioV.java
@@ -0,0 +1,24 @@
+package OpenWebiners;
+
+import java.util.Scanner;
+
+/**
+ *  Calcular la conversion de Euros a Dolares.
+ */
+public class ejercicioV {
+    public static void main(String[] args) {
+        double Euro = 0 ;
+        double Dolar = 1.88 ;
+        double conversion = 0 ; 
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Dime cuantos Euros te convierto en Dolares ");
+        Euro = Integer.parseInt(sc.nextLine());
+
+        conversion = Euro * Dolar ;
+        System.out.println( Euro  + " Euros son " + conversion + " Dolares " );
+
+        sc.close();
+    }
+
+    
+}
---
+++ OpenWebiners/ejercicioVI.java
@@ -0,0 +1,26 @@
+package OpenWebiners;
+
+import java.util.Scanner;
+
+/**
+ * Calcular si un numero es par o impar 
+ */
+
+public class ejercicioVI {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int numero = 0 ;
+        System.out.println(" Introduce un numero que te dire si es par o impar ");
+        numero = Integer.parseInt(sc.nextLine());
+
+        if (numero % 2 == 0) {
+            System.out.println(" Es un numero par ");
+        } else{
+            System.out.println(" Es un numero impar ");
+        }
+        
+        sc.close();
+    }
+
+}
