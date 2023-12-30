39dc681bb8ebf4a453c47f71aef8ede64286b328
Topeto14 <levishurtadorobert@gmail.com>
2023-12-30 15:14:16 +0100

    fchgfgh

--- OpenWebiners/ejercicioIV.java
+++ OpenWebiners/ejercicioIV.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class ejercicioIV {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         double area = 0 ;
         double perimetro = 0 ;
-        double PI = 3.14 ;
         double radio = 0 ;
         System.out.println(" Te voy a calcular el area y el perimetro de un circulo ");
         System.out.println(" Dime el radio del circulo ");
         radio = Integer.parseInt(sc.nextLine());
 
-        area = PI * (radio*radio) ; 
-        perimetro = 2 * PI * radio ; 
+        area = Math.PI * (radio*radio) ; 
+        perimetro = 2 * Math.PI * radio ; 
 
         System.out.println(" El area del circulo es : " + area ) ;
         System.out.println(" El perimetro del circulo es : " + perimetro );
