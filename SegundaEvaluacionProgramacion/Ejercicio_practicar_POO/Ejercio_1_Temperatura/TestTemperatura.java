58ff22fd019c598f312c25750fe8241b21b532ad
levishurtadorobert@gmail.com <rlevhur373@g.educaand.es>
2024-01-11 18:30:36 +0100

    gfdsgsdgf

--- SegundaEvaluacionProgramacion/Ejercicio_practicar_POO/Ejercio_1_Temperatura/TestTemperatura.java
+++ SegundaEvaluacionProgramacion/Ejercicio_practicar_POO/Ejercio_1_Temperatura/TestTemperatura.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TestTemperatura {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        System.out.println(new Temperatura().celciusToFarenheit);
 
 
     }
